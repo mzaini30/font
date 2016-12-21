0a27c68077852e00883a90c916aa3abd3d6f697d
mzaini30 <muhzaini30@gmail.com>
2016-12-21 16:26:03 +0800

    otomatis font

---
+++ data-font.js
@@ -0,0 +1,11 @@
+font = [
+"candlescript-demo-version.otf",
+"dragon-is-coming.otf",
+"legion-of-darwin.ttf",
+"mademoiselle-camille.ttf",
+"maratre.otf",
+"pamega-demo.ttf",
+"tasty-birds.otf",
+"the-heart-of-everything-demo.ttf",
+"vintage-avalanche.otf"
+]
