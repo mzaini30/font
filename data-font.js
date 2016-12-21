425ec4f235b3a752b38d0d2de3cdeea8706a2389
mzaini30 <muhzaini30@gmail.com>
2016-12-21 16:57:37 +0800

    tambah 5 font

--- data-font.js
+++ data-font.js
@@ -1,11 +1,16 @@
 font = [
 "candlescript-demo-version.otf",
 "dragon-is-coming.otf",
+"happy.ttf",
 "legion-of-darwin.ttf",
 "mademoiselle-camille.ttf",
 "maratre.otf",
+"night.ttf",
 "pamega-demo.ttf",
+"paperdaisy-demo.ttf",
+"pretty.ttf",
 "tasty-birds.otf",
 "the-heart-of-everything-demo.ttf",
-"vintage-avalanche.otf"
+"vintage-avalanche.otf",
+"wizard.ttf"
 ]
